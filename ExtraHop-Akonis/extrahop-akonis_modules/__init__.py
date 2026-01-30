7df75ccacbd213e6820aa284637a90cbf8e5fafe
Hakim <helkabouss@akonis.fr>
2026-01-30 11:01:35 +0100

    Add ExtraHop-Akonis module with generated files

---
+++ ExtraHop-Akonis/extrahop-akonis_modules/__init__.py
@@ -0,0 +1,6 @@
+from sekoia_automation.module import Module
+from extrahop-akonis_modules.models import Extrahop-AkonisModuleConfiguration
+
+
+class Extrahop-AkonisModule(Module):
+    configuration: Extrahop-AkonisModuleConfiguration
---
+++ ExtraHop-Akonis/main.py
@@ -0,0 +1,5 @@
+from extrahop-akonis_modules import Extrahop-AkonisModule
+
+if __name__ == "__main__":
+    module = Extrahop-AkonisModule()
+    module.run()
